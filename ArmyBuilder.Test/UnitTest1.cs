f3efe77df6c71e689c307dda1822ed8591e1e529
slycoder <slycoder127@hotmail.com>
2016-10-07 16:40:47 -0400

    added unit test

---
+++ ArmyBuilder.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArmyBuilder.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestArmyDataLoad()
+        {
+            string path = "";
+            var stream = new FileStream(path, FileMode.Open);
+
+        }
+    }
+}
