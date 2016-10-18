fce43c85ec0ce12bb379acd75c926a9dbf74a2b2
slycoder <slycoder127hotmail.com>
2016-10-17 21:25:40 -0400

    windows ui update

--- ArmyBuilder/ArmyBuilder.Core/ViewModels/MainViewModel.cs
+++ ArmyBuilder/ArmyBuilder.Core/ViewModels/MainViewModel.cs
@@ -19,19 +19,16 @@ namespace ArmyBuilder.Core.ViewModels
 {
     public class MainViewModel : BindableBase
     {
-        public ArmyListViewModel CurrentArmyListViewModel { get { return currentArmyListViewModel; } private set { SetValue(ref currentArmyListViewModel, value); } }
         public ArmyList SelectedArmyList { get { return selectedArmyList; } set { SetValue(ref selectedArmyList, value); } }
 
         public ObservableCollection<ArmyList> ArmyLists => database.ArmyLists;
-        public ObservableCollection<ArmyListGroup> ArmyListGroups { get; } = new ObservableCollection<ArmyListGroup>();
         public ObservableCollection<Army> Armies { get; } = new ObservableCollection<Army>();
 
-        public RelayCommand<ArmyList> RemoveSelectedListCommand => new RelayCommand<ArmyList>(RemoveSelectedList);
-        public RelayCommand EditSelectedListCommand => new RelayCommand(EditSelectedArmyList);
+        public RelayCommand<ArmyList> RemoveListCommand => new RelayCommand<ArmyList>(RemoveSelectedList);
+        public RelayCommand AddListCommand => new RelayCommand(AddList);
 
         public static SynchronizationContext UiContext; 
         private readonly ArmyBuilderDatabase database = new ArmyBuilderDatabase();
-        private ArmyListViewModel currentArmyListViewModel;
         private ArmyList selectedArmyList;
 
         // ReSharper disable once EmptyConstructor
@@ -66,11 +63,7 @@ namespace ArmyBuilder.Core.ViewModels
                     
                     ArmyLists.Where(a => a.ArmyId == army.Id).ForEach(a => a.Army = army);
                 }
-
-                ArmyLists.CollectionChanged += (sender, args) => { UpdateListSource(false); };
-                UpdateListSource(true);
             }
-            
         }
 
         private void RemoveSelectedList(ArmyList list)
@@ -78,26 +71,10 @@ namespace ArmyBuilder.Core.ViewModels
             ArmyLists?.Remove(list);
         }
 
-        public void AddList(ArmyList newList)
+        private void AddList()
         {
-            ArmyLists?.Add(newList);
-            SelectedArmyList = newList;
-        }
-
-        public void EditSelectedArmyList()
-        {
-            CurrentArmyListViewModel = null;
-
-            if (SelectedArmyList != null)
-            {
-                CurrentArmyListViewModel = new ArmyListViewModel(selectedArmyList);
-                database.ArmyListData.Where(a=>a.ArmyListId == SelectedArmyList.Id).ForEach(a =>
-                {
-                    a.SetData(SelectedArmyList.Army.UnitEntries.Single(u => u.Id == a.UnitId));
-                    //CurrentArmyListViewModel.UnitEntries.Add(a);
-                });
-                //CurrentArmyListViewModel.UnitEntries.CollectionChanged += UnitsOnCollectionChanged;
-            }
+            ArmyLists.Add(new ArmyList { Id = Guid.NewGuid(), Name = "New List", PointsLimit = 1500});
+            SelectedArmyList = ArmyLists.Last();
         }
 
         private void UnitsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -107,29 +84,12 @@ namespace ArmyBuilder.Core.ViewModels
                 case NotifyCollectionChangedAction.Add:
                     e.NewItems.Cast<ArmyListData>().ForEach(d=> database.ArmyListData.Add(d));
                     break;
-                case NotifyCollectionChangedAction.Move:
-                    break;
                 case NotifyCollectionChangedAction.Remove:
                     e.OldItems.Cast<ArmyListData>().ForEach(d => database.ArmyListData.Remove(d));
-                    break;
-                case NotifyCollectionChangedAction.Replace:
-                    break;
-                case NotifyCollectionChangedAction.Reset:
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-        }
-
-        private void UpdateListSource(bool cacheSelection)
-        {
-            var currentSelectedItem = SelectedArmyList;
-            var groups = ArmyLists?.OrderBy(i => i.Army.Name).GroupBy(i => new {i.ArmyId}).Select(i => new ArmyListGroup(i.ToList())).ToList();
-
-            ArmyListGroups.Clear();
-            groups.ForEach(g => ArmyListGroups.Add(g));
-   
-            SelectedArmyList = cacheSelection ? currentSelectedItem : null;
         }
     }
 }
