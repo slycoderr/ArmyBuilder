1499f058f7582535a2d438a6720699fadc980440
slycoder <slycoder127@hotmail.com>
2016-10-19 17:00:57 -0400

    added mobile projects

--- ArmyBuilder.XamarianForms/ArmyBuilder.XamarianForms/Properties/AssemblyInfo.cs
+++ ArmyBuilder.XamarianForms/ArmyBuilder.XamarianForms/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Slycoder")]
+[assembly: AssemblyTitle("ArmyBuilder.XamarianForms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Slycoder")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ArmyBuilder.XamarianForms")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.34")]
+[assembly: AssemblyVersion("1.0.0.3")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ArmyBuilder.Xamarin/ArmyBuilder.Xamarin/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ArmyBuilder.Xamarin
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ArmyBuilder.Xamarin.Page1();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
