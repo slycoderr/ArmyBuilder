0d396ccdc844d0ac7af2ceabef811e7045e41ea8
Aaron Kerti <slycoder127@hotmail.com>
2016-11-22 19:01:42 -0500

    added unit test

---
+++ ArmyBuilder.Test/MainViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ArmyBuilder.Core.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArmyBuilder.Test
+{
+    [TestClass]
+    public class MainViewModelTests
+    {
+        [TestMethod]
+        public void TestArmyDataLoad()
+        {
+            MainViewModel mv = new MainViewModel();
+            string path = "C:\\Users\\slyco\\Source\\Repos\\armybuilder\\ARmyData\\Sylvaneth.xml";
+            int checkArmyId = 10;
+            int numUnits = 11;
+
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                mv.LoadArmyData(stream);
+            }
+
+            var army = mv.Armies.First(a => a.Id == checkArmyId); //make sure the army is there
+
+            Assert.AreEqual(numUnits, army.Units.Count);
+            Assert.AreEqual(120, army.Units.First().BaseCost);
+            Assert.AreEqual(10, army.Units.First().Minimum);
+            Assert.AreEqual(30, army.Units.First().Maximum);
+            Assert.AreEqual(120, army.Units.First().IncrementCost);
+
+        }
+    }
+}
